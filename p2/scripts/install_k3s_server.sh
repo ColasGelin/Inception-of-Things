356a677bf66fc373f59f56e362c49776fc7ac837
vboxuser <vboxuser@IOT.myguest.virtualbox.org>
2026-07-14 16:18:26 +0200

    p1 done and p2 getting checked but should be done

--- p2/scripts/install_k3s_server.sh
+++ p2/scripts/install_k3s_server.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
 set -e
 
-apt-get update
-apt-get install -y curl
+SERVER_IP="192.168.56.110"
 
-curl -sfL https://get.k3s.io | sh -
+apt-get update && apt-get install -y net-tools curl
+
+curl -sfL https://get.k3s.io | \
+INSTALL_K3S_EXEC="server --node-ip=${SERVER_IP} --tls-san=${SERVER_IP}" \
+sh -
+
+ln -sf /usr/local/bin/k3s /usr/local/bin/kubectl
 
 mkdir -p /var/lib/rancher/k3s/server/manifests
 cp /vagrant/apps.yaml /var/lib/rancher/k3s/server/manifests/apps.yaml
